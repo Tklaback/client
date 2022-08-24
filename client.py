b169a17ee44e7f6acefd0a7683154a86ac4b5bc5
Ty R. Klabacka <klabacka.ty@gmail.com>
2022-08-24 09:43:34 -0600

    Needed to adjust ip address of quest

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket 
+from datetime import datetime
+from playsound import playsound
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('192.168.1.27', 65432))
+    s.connect(('192.168.1.16', 65432)) #'10.0.0.126'
     running = True
     while running:
         data = s.recv(1024)
-        if data:    
-            print(data.decode())
+        if data: 
+            playsound("test.wav")
+            now = datetime.now().time()
+            print((now, data.decode()))
         else:
             break
